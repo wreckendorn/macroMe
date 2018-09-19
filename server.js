eb7b89d8e38d498d210a238d8ebd9ce4f2745354
Hecky <thechrisheckendorn@gmail.com>
2018-09-18 19:56:21 -0500

    adding env

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(bodyParser.json());
 // passport.deserializeUser(Account.deserializeUser());
 
 //===============
-
+// if(process.env.NODE_ENV !== 'production') require('../secrets');
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
